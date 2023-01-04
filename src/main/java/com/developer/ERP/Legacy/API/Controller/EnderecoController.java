65cb13e5651d489b26901653ffb9ed9afb395caa
FireXtz <phantomrising@protonmail.com>
2023-01-04 00:10:41 -0300

    controller de enderecos adcionado

---
+++ src/main/java/com/developer/ERP/Legacy/API/Controller/EnderecoController.java
@@ -0,0 +1,40 @@
+package com.developer.ERP.Legacy.API.Controller;
+
+import com.developer.ERP.Legacy.API.Model.Endereco;
+import com.developer.ERP.Legacy.API.Service.EnderecoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/enderecos")
+public class EnderecoController {
+
+    @Autowired
+    private EnderecoService enderecoService;
+
+    @GetMapping
+    public List <Endereco>listarTodos(){
+        return enderecoService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity <Endereco>listarPor(@PathVariable Long id){
+        return enderecoService.listarPor(id);
+    }
+
+    @PutMapping("/{id}")
+    public Endereco editar(@PathVariable Long id,@RequestBody Endereco endereco){
+        return enderecoService.editar(endereco, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Map<String, Boolean>> remover(@PathVariable Long id){
+        return enderecoService.remover(id);
+    }
+
+
+}
