6c11fa7631c7fa835d48d3e986df5b8d5f09586b
JarmisonS10 <jarmison.paiva@varejonline.com.br>
2023-08-18 16:07:20 -0300

    melhoria nos filtros, criado um customRepositoryImpl para criar implementação de paginação

--- src/main/java/com/developer/ERP/Legacy/API/api/controller/ClienteController.java
+++ src/main/java/com/developer/ERP/Legacy/API/api/controller/ClienteController.java
@@ -4,8 +4,12 @@ import com.developer.ERP.Legacy.API.domain.model.Cliente;
 import com.developer.ERP.Legacy.API.domain.repository.criteriaFilter.ClienteCriteriaFilter;
 import com.developer.ERP.Legacy.API.domain.repository.filter.ClienteFilter;
 import com.developer.ERP.Legacy.API.domain.service.ClienteService;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,5 +29,11 @@ public class ClienteController {
     public Cliente salvarCliente(@RequestBody Cliente cliente) throws Exception{
     	return clienteService.cadastrarCliente(cliente);
     }
+    @GetMapping("contratos-vinculados/{id}")
+    public Page<Cliente>buscarContratosClienteByCriteriaFilterPaginator(ClienteCriteriaFilter clienteFilter, Pageable pageable, @PathVariable Long id){
+    	return clienteService.buscarContratosClienteByCriteriaFilterPaginator(clienteFilter, pageable);
+    }
+    
+    
   
 }
