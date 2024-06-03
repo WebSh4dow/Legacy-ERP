1ee6c53e69a9c6c7d9122dc6d43ac80ebb0ec6be
jarmison.paiva <jarmison6@gmail.com>
2024-06-03 13:59:15 -0300

    adicionado querys iniciais de consulta por cpf, nome e profissao

--- src/main/java/com/developer/ERP/Legacy/API/infrastructure/adapters/output/persistence/repository/ProprietarioRepository.java
+++ src/main/java/com/developer/ERP/Legacy/API/infrastructure/adapters/output/persistence/repository/ProprietarioRepository.java
@@ -1,9 +1,24 @@
 package com.developer.ERP.Legacy.API.infrastructure.adapters.output.persistence.repository;
 
+import com.developer.ERP.Legacy.API.domain.model.Proprietario;
 import com.developer.ERP.Legacy.API.infrastructure.adapters.output.persistence.entity.ProprietarioEntity;
+import com.developer.ERP.Legacy.API.infrastructure.adapters.output.persistence.query.ProprietarioQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface ProprietarioRepository extends JpaRepository<ProprietarioEntity, Long> {
+import java.util.List;
+
+public interface ProprietarioRepository extends JpaRepository<ProprietarioEntity, Long>, ProprietarioQuery {
     boolean existsProprietarioByCpfAndRgIgnoreCase(String cpf, String rg);
+
+    @Query(value = CONSULTAR_PROPRIETARIO_POR_CPF)
+    Proprietario consultarProprietarioPorCpf(String cpf);
+
+    @Query(value = CONSULTAR_PROPRIETARIO_PROFISSAO)
+    List<Proprietario> consultarProprietariosPorProfissao(String profissao);
+
+    @Query(value = CONSULTAR_PROPRIETARIO_POR_NOME)
+    List<Proprietario> consultarProprietarioPorNome(String nome);
+
 
 }
